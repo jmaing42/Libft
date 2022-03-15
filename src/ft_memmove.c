faf4f85913e823f5bb8b7467bc85709a672bd910
Juyeong Maing <mjy9088@naver.com>
2022-03-15 05:10:06 +0000

    memmove - merge into one function

--- src/ft_memmove.c
+++ src/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jmaing <jmaing@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 13:34:22 by jmaing            #+#    #+#             */
-/*   Updated: 2022/03/08 21:41:43 by jmaing           ###   ########.fr       */
+/*   Updated: 2022/03/15 05:05:40 by jmaing           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,16 @@
 
 #include "ft.h"
 
-static void	*ft_memrcpy(void *dest, void *source, size_t len)
+void	*ft_memmove(void *dest, void *source, size_t len)
 {
 	char	*src;
 	char	*dst;
 
+	if (dest < source || (uintptr_t) source + len < (uintptr_t) dest)
+		return (ft_memcpy(dest, (void *) source, len));
 	src = (char *) source + len;
 	dst = (char *) dest + len;
 	while (len--)
 		*dst-- = *src--;
 	return (dest);
-}
-
-void	*ft_memmove(void *dest, void *source, size_t len)
-{
-	if (source < dest && (uintptr_t) source + len > (uintptr_t) dest)
-		return (ft_memrcpy(dest, (void *) source, len));
-	return (ft_memcpy(dest, (void *) source, len));
 }
