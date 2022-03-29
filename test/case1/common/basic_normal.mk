6f752f16ec39cd180c28277755b10c59e2ebd4a4
맹주영 <mjy9088@naver.com>
2022-03-29 21:16:12 +0900

    add valgrind test

--- test/case1/common/basic_normal.mk
+++ test/case1/common/basic_normal.mk
@@ -2,3 +2,8 @@ ADDITIONAL_CC_LD_FLAGS := -g3
 LIBFT_PATH := ../../../../libft_normal.a
 
 include ../../../../common/basic_common.mk
+
+test: test_with_valgrind
+test_with_valgrind:
+	$(Q)[[ ! command -v valgrind ]] || valgrind ./tester
+.PHONY: test_with_valgrind
