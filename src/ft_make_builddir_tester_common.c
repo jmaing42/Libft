1b2ca69b966ccd7bb6ad1b44678613875ba249ae
Juyeong Maing <mjy9088@naver.com>
2022-03-30 13:53:05 +0000

    add test format...?

---
+++ src/ft_make_builddir_tester_common.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_make_builddir_tester_common.c                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmaing <jmaing@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/30 12:54:40 by jmaing            #+#    #+#             */
+/*   Updated: 2022/03/30 13:26:09 by jmaing           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+static const char *const	g_makefile_contents = "" \
+	"all: tester.a\n" \
+	"tester.a: tester.o\n" \
+	"	ar cr tester.a tester.o\n" \
+	"tester.o: tester.c\n" \
+	"	gcc -o tester.o -Wall -Wextra -Werror -c tester.c\n" \
+	"tester.c:\n" \
+	"	echo \"" \
+				"int main() { return 0; }" \
+			"\" > tester.c\n" \
+	"";
+
+int	main(void)
+{
+	printf("%s", g_makefile_contents);
+	return (0);
+}
