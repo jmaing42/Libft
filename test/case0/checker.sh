211a95c4aa346a910b6d85da7afa49e0e0c15a89
맹주영 <mjy9088@naver.com>
2022-04-01 20:17:05 +0900

    check Makefile, liibft.h

--- test/case0/checker.sh
+++ test/case0/checker.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if ! ./checker "$1"; then
+  echo "File $1 has disallowed characters"
+fi
+
 if ! norminette "$1" > "$1.norm.txt"; then
   echo "Norm error on $1"
   cat "$1.norm.txt"
@@ -10,8 +14,4 @@ if ! gcc -c -o "$1.o" -c "$1" -Wall -Wextra -Werror > "$1.compile.log"; then
   echo "Failed to compile $1"
   cat "$1.compile.log"
   exit 1
-fi
-
-if ! ./checker "$1"; then
-  echo "File $1 has disallowed characters"
 fi
