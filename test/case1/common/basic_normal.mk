2402849d88a8f85173cc70bfe9d1500fa48250fd
맹주영 <mjy9088@naver.com>
2022-04-02 21:00:12 +0900

    use valgrind properly

--- test/case1/common/basic_normal.mk
+++ test/case1/common/basic_normal.mk
@@ -5,5 +5,5 @@ include $(BASE_PATH)/common/basic_common.mk
 
 test: test_with_valgrind
 test_with_valgrind:
-	$Q(! command -v valgrind) || valgrind ./tester
+	$Q(! command -v valgrind) || valgrind -q --leak-check=full ./tester
 .PHONY: test_with_valgrind
