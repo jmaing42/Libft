80eed3812144bce557f621f97da8233455519389
Juyeong Maing <mjy9088@naver.com>
2022-04-06 17:14:29 +0000

    add bonus

---
+++ src/ft_src_part3_advanced_bonus.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_src_part3_advanced_bonus.c                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmaing <jmaing@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/06 17:03:16 by jmaing            #+#    #+#             */
+/*   Updated: 2022/04/06 17:11:53 by jmaing           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	del(lst->content);
+	free(lst);
+}
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*next;
+
+	while (*lst)
+	{
+		next = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = next;
+	}
+}
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*result;
+	t_list	*new_node;
+
+	result = NULL;
+	while (lst)
+	{
+		new_node = ft_lstnew(f(lst->content));
+		lst = lst->next;
+		if (!new_node)
+		{
+			ft_lstclear(&result, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&result, new_node);
+	}
+	return (result);
+}
---
+++ src/ft_src_part3_basic_bonus.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_src_part3_basic_bonus.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmaing <jmaing@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/06 16:56:41 by jmaing            #+#    #+#             */
+/*   Updated: 2022/04/06 17:09:40 by jmaing           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list *const	result = (t_list *) malloc(sizeof(t_list));
+
+	if (!result)
+		return (NULL);
+	result->next = NULL;
+	result->content = content;
+	return (result);
+}
+
+void	ft_lstadd_front(t_list **lst, t_list *node)
+{
+	node->next = *lst;
+	*lst = node;
+}
+
+int		ft_lstsize(t_list *lst)
+{
+	int	result;
+
+	result = 0;
+	while (lst)
+	{
+		result++;
+		lst = lst->next;
+	}
+	return (result);
+}
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*result;
+
+	result = lst;
+	while (result && result->next)
+		result = result -> next;
+	return (result);
+}
+
+void	ft_lstadd_back(t_list **lst, t_list *node)
+{
+	if (!*lst)
+		*lst = node;
+	else
+		ft_lstlast(*lst)->next = node;
+}
