3f7651364c34d081d1546aeca5f4dce6f685af01
맹주영 <mjy9088@naver.com>
2022-04-06 22:09:51 +0900

    copy split from piscine

--- src/ft_src_part2_split.c
+++ src/ft_src_part2_split.c
@@ -6,39 +6,23 @@
 /*   By: jmaing <jmaing@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 05:37:46 by jmaing            #+#    #+#             */
-/*   Updated: 2022/03/31 13:09:28 by jmaing           ###   ########.fr       */
+/*   Updated: 2022/04/06 22:09:35 by jmaing           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdint.h>
-#include <stdbool.h>
 #include <stdlib.h>
 
-#include "libft.h"
-
-// TODO:
-
-static void	ft_array_free(char **arr)
+static char	*ft_strndup(char *src, int max_len)
 {
-	char	**tmp;
-
-	tmp = arr - 1;
-	while (*++tmp)
-		free(tmp);
-	free(arr);
-}
-
-static char	*ft_strndup(const char *src, size_t max_len)
-{
-	size_t	length;
+	int		length;
 	char	*result;
 	char	*tmp;
 
-	tmp = (char *) src;
+	tmp = src;
 	length = 0;
 	while (*tmp++ && length++ < max_len)
 		;
-	result = (char *) malloc(length + 1);
+	result = (char *) malloc((length + 1) * sizeof(char));
 	if (!result)
 		return (NULL);
 	tmp = result;
@@ -49,31 +33,54 @@ static char	*ft_strndup(const char *src, size_t max_len)
 	return (result);
 }
 
-static const char	*ft_string_find(const char *str, char c, bool inverse)
+static char	*ft_string_find_one_of(char *str, char *charset)
 {
+	char	*tmp;
+
 	str--;
 	while (*++str)
-		if ((*str != c) != inverse)
-			return (str);
+	{
+		tmp = charset - 1;
+		while (*++tmp)
+			if (*tmp == *str)
+				return (str);
+	}
 	return (str);
 }
 
-static int	ft_split_internal_alloc(const char *str, char c, char ***out)
+static char	*ft_string_find_not_one_of(char *str, char *charset)
+{
+	char	*tmp;
+
+	str--;
+	while (*++str)
+	{
+		tmp = charset - 1;
+		while (*++tmp)
+			if (*tmp == *str)
+				break ;
+		if (!*tmp)
+			return (str);
+	}
+	return (str);
+}
+
+static int	ft_split_internal_alloc(char *str, char *charset, char ***out)
 {
 	char	**result;
-	size_t	length;
+	int		length;
 
 	length = 0;
-	str = ft_string_find(str, c, true);
+	str = ft_string_find_not_one_of(str, charset);
 	while (*str)
 	{
 		length++;
-		str = ft_string_find(str, c, false);
+		str = ft_string_find_one_of(str, charset);
 		if (!*str)
 			break ;
-		str = ft_string_find(str, c, true);
+		str = ft_string_find_not_one_of(str, charset);
 	}
-	result = ft_calloc(length + 1, sizeof(char *));
+	result = malloc(sizeof(char *) * (length + 1));
 	if (!result)
 		return (1);
 	result[length] = NULL;
@@ -81,29 +88,26 @@ static int	ft_split_internal_alloc(const char *str, char c, char ***out)
 	return (0);
 }
 
-char	**ft_split(const char *str, char c)
+char	**ft_split(char *str, char c)
 {
-	char		**result;
-	const char	*tmp;
-	size_t		index;
+	char	**result;
+	char	*tmp;
+	int		index;
+	char	charset[2];
 
-	if (ft_split_internal_alloc(str, c, &result))
+	charset[0] = c;
+	charset[1] = '\0';
+	if (ft_split_internal_alloc(str, charset, &result))
 		return (NULL);
 	index = 0;
-	str = ft_string_find(str, c, true);
+	str = ft_string_find_not_one_of(str, charset);
 	while (*str)
 	{
-		tmp = ft_string_find(str, c, false);
-		result[index] = ft_strndup(str, (uintptr_t) tmp - (uintptr_t) str);
-		if (!result[index])
-		{
-			ft_array_free(result);
-			return (NULL);
-		}
-		index++;
+		tmp = ft_string_find_one_of(str, charset);
+		result[index++] = ft_strndup(str, tmp - str);
 		if (!*tmp)
 			break ;
-		str = ft_string_find(tmp, c, true);
+		str = ft_string_find_not_one_of(tmp, charset);
 	}
 	return (result);
 }
