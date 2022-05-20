40a5154d1edbf628033abf88fbbe94c873f8f404
맹주영 <mjy9088@naver.com>
2022-05-20 19:45:01 +0900

    add strlen

--- test/case1/part1/ft_strlen/main.c
+++ test/case1/part1/ft_strlen/main.c
@@ -6,7 +6,7 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 00:54:54 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/05/20 19:39:57 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/05/20 19:44:32 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,29 +18,25 @@
 #define BUFFER_SIZE 1024
 #define TEST_COUNT 4242
 
-extern void	*ft_memset(void *mem, int value, size_t len);
+extern size_t	ft_strlen(const char *str);
 
 int	main(void)
 {
 	unsigned int	i;
 	size_t			j;
-	size_t			k;
 	unsigned char	buffer[BUFFER_SIZE];
-	unsigned char	copy[BUFFER_SIZE];
 
 	i = 0;
 	while (i++ < TEST_COUNT)
 	{
 		srand(i);
 		j = 0;
-		while (j < BUFFER_SIZE)
-			buffer[j++] = rand() % (1 << CHAR_BIT);
-		memcpy(copy, buffer, BUFFER_SIZE);
-		j = rand() % (BUFFER_SIZE / 2);
-		k = rand() % (BUFFER_SIZE - j);
-		ft_memset(buffer + j, (char)((i * i) % (UCHAR_MAX)), k + 1);
-		memset(copy + j, (char)((i * i) % (UCHAR_MAX)), k + 1);
-		if (memcmp(buffer, copy, BUFFER_SIZE))
+		while (j + 1 < BUFFER_SIZE)
+			buffer[j++] = rand() % UCHAR_MAX + 1;
+		buffer[j] = '\0';
+		j = rand() % (BUFFER_SIZE - 1);
+		if (ft_strlen((const char *)(&buffer[j]))
+			!= strlen((const char *)(&buffer[j])))
 			return (EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
